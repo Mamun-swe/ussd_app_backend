e468c8d2caef196ff77eec6c1a5f919750bb8d79
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-06-25 17:59:39 +0600

    comment

--- api/controllers/admin/doctor.js
+++ api/controllers/admin/doctor.js
@@ -36,15 +36,79 @@ const createDoctor = async (req, res) => {
 const doctorsList = async (req, res) => {
     try {
         let doctors
-        let message
-        let doctorAll = Doctor.find().sort({ _id: -1 })
+        let doctorAll = await Doctor.find({}, { role: 0, createdAt: 0, updatedAt: 0 }).sort({ _id: -1 })
         res.status(200).json({
             doctors: doctorAll
         })
     } catch (error) {
         if (error) {
             res.status(500).json({
-                message: error
+                error
+            })
+        }
+    }
+}
+
+
+const doctorView = async (req, res) => {
+    try {
+        let doctor
+        let id = req.params.id
+
+        const result = await Doctor.findById({ _id: id }, { role: 0, createdAt: 0, updatedAt: 0 })
+        res.json({
+            doctor: result
+        })
+
+    } catch (error) {
+        if (error) {
+            res.status(204).json({
+                error
+            })
+        }
+    }
+}
+
+
+const doctorDelete = async (req, res) => {
+    try {
+        let message
+        let id = req.params.id
+
+        const result = await Doctor.findByIdAndRemove({ _id: id })
+        res.json({
+            message: true
+        })
+
+    } catch (error) {
+        if (error) {
+            res.status(204).json({
+                error
+            })
+        }
+    }
+}
+
+
+const doctorUpdate = async (req, res) => {
+    let message
+    let id = req.params.id
+    const data = req.body
+    try {
+        const result = await Doctor.findByIdAndUpdate(
+            { _id: id },
+            { $set: data },
+            { new: true }
+        )
+
+        res.status(200).json({
+            message: true
+        })
+
+    } catch (error) {
+        if (error) {
+            res.status(204).json({
+                error
             })
         }
     }
@@ -53,5 +117,8 @@ const doctorsList = async (req, res) => {
 
 module.exports = {
     createDoctor,
-    doctorsList
+    doctorsList,
+    doctorView,
+    doctorDelete,
+    doctorUpdate
 }
--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,7 +2,10 @@ const express = require("express")
 const router = express.Router()
 const doctorController = require("../controllers/admin/doctor")
 
-router.post("/doctor/create", doctorController.createDoctor)
-router.get("/doctors", doctorController.doctorsList)
+router.post("/doctor", doctorController.createDoctor)
+router.get("/doctor", doctorController.doctorsList)
+router.get("/doctor/:id/view", doctorController.doctorView)
+router.delete("/doctor/:id/delete", doctorController.doctorDelete)
+router.put("/doctor/:id/update", doctorController.doctorUpdate)
 
 module.exports = router
